d3ea2b6f04e572899461baf3257d666d02f2141a
AyushThk <139217623+AyushThk@users.noreply.github.com>
2023-08-18 23:19:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MySQL Connection and Display</title>
+</head>
+<body>
+
+<?php
+$hostname = 'p3plzcpnl492196.prod.phx3.secureserver.net'; // or the hostname provided by your hosting
+$username = 'ejz3gf3j1zau'; // your MySQL username
+$password = 'niriadmin@2023'; // your MySQL password
+$database = 'niridb'; // your MySQL database name
+
+// Create a connection
+$con = mysqli_connect($hostname,$username,$password,$database);
+
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
+echo "Connected successfully<br>";
+
+// Fetch and display data
+$query = "SELECT * FROM mytest"; // Replace 'your_table' with your actual table name
+
+$result = $con->query($query);
+
+if ($result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row['id'] . "</td><td>" . $row['name'] . "</td><td>" . $row['email'] . "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No records found";
+}
+
+// Close the connection
+$con->close();
+?>
+
+</body>
+</html>
